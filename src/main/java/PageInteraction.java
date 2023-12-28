f0dbfc6a90e51927fe891ead640c0ca85c1c930d
vitor <vitorhugosilva2009@hotmail.com>
2023-12-27 23:36:17 -0300

    Digitando texto completo
    Logica/gambiarra para digitar o texto quase completo

--- src/main/java/PageInteraction.java
+++ src/main/java/PageInteraction.java
@@ -1,21 +1,40 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PageInteraction {
-    private static final String TARGET_URL = "https://www.ratatype.com/typing-test/";
     private static final String TEXT_ELEMENT_ID = "testText";
+
     public static String getTextContent(WebDriver driver) {
-        driver.get(TARGET_URL);
         var wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(TEXT_ELEMENT_ID)));
-
         try {
-            return driver.findElement(By.id(TEXT_ELEMENT_ID)).getText();
+            WebElement typingArea = driver.findElement(By.id(TEXT_ELEMENT_ID));
+            typingArea.click();
+            return typingArea.getText();
         } catch (Exception e) {
             System.out.println("Error retrieving text content: " + e.getMessage());
             return "";
+        }
+    }
+
+    public static void closeTooltipLanguage(WebDriver webDriver) {
+        try {
+
+            var wait = new WebDriverWait(webDriver, 5);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div/div/button")));
+            WebElement tooltipLanguage =
+                    webDriver.findElement(By.xpath("/html/body/div[3]/div/div/button"));
+
+            if (tooltipLanguage != null) {
+                Thread.sleep(1000);
+
+                tooltipLanguage.click();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/TypingHandler.java
+++ src/main/java/TypingHandler.java
@@ -1,24 +1,49 @@
+import org.openqa.selenium.WebDriver;
+
+import javax.swing.*;
 import java.awt.Robot;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class TypingHandler {
-    public static void typeTextWithRobot(Robot robot, String text) {
+    public static void typeTextWithRobot(Robot robot, String text, WebDriver driver) throws InterruptedException {
         var shiftPressed = false;
+        String old = "";
+        int qt = 8;
+        // digitar até  o certificado ou o texto do certificado aparecer na tela
+        while (qt != 0) {
+            Thread.sleep(10000);
+            if (qt != 8) {
+                System.out.println("-------");
+                System.out.println("old");
+                System.out.println(old.replace("\r\n",""));
+                text = PageInteraction.getTextContent(driver);
+                text = text.replace(old,"");
+                System.out.println("New Text");
+                System.out.println(text);
+            }
+            for (int i = 0; i < text.length(); i++) {
+                var currentChar = text.charAt(i);
 
-        for (int i = 0; i < text.length(); i++) {
-            var currentChar = text.charAt(i);
+                if (Character.isUpperCase(currentChar) && !shiftPressed) {
+                    robot.keyPress(java.awt.event.KeyEvent.VK_SHIFT);
+                    shiftPressed = true;
+                } else if (!Character.isUpperCase(currentChar) && shiftPressed) {
+                    robot.keyRelease(java.awt.event.KeyEvent.VK_SHIFT);
+                    shiftPressed = false;
+                }
 
-            if (Character.isUpperCase(currentChar) && !shiftPressed) {
-                robot.keyPress(java.awt.event.KeyEvent.VK_SHIFT);
-                shiftPressed = true;
-            } else if (!Character.isUpperCase(currentChar) && shiftPressed) {
-                robot.keyRelease(java.awt.event.KeyEvent.VK_SHIFT);
-                shiftPressed = false;
+                var keyCode = java.awt.event.KeyEvent.getExtendedKeyCodeForChar(currentChar);
+                robot.keyPress(keyCode);
+                robot.keyRelease(keyCode);
+                Thread.sleep(10);
             }
 
-            var keyCode = java.awt.event.KeyEvent.getExtendedKeyCodeForChar(currentChar);
-            robot.keyPress(keyCode);
-            robot.keyRelease(keyCode);
+            old += text;
+            qt--;
         }
+
 
         if (shiftPressed) {
             robot.keyRelease(java.awt.event.KeyEvent.VK_SHIFT);
